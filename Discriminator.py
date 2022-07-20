087c714eb7c2f4f5f4e2613d9b3720cb4b688d55
Joel Turkulainen <Neuroosi@users.noreply.github.com>
2022-07-20 12:39:12 +0300

    Add files via upload

--- Discriminator.py
+++ Discriminator.py
@@ -10,16 +10,18 @@ class Discriminator(nn.Module):
     def __init__(self, input_dim):
         super(Discriminator, self).__init__()
         self.fc1 = nn.Linear(input_dim, 100)
-        torch.nn.init.kaiming_uniform_(self.fc1.weight)
+        #torch.nn.init.kaiming_uniform_(self.fc1.weight)
         self.fc2 = nn.Linear(100, 100)
-        torch.nn.init.kaiming_uniform_(self.fc2.weight)
+        #torch.nn.init.kaiming_uniform_(self.fc2.weight)
         self.fc3 = nn.Linear(100, 1)
-        torch.nn.init.xavier_uniform_(self.fc3.weight)
+        #torch.nn.init.xavier_uniform_(self.fc3.weight)
         self.logsigmoid = nn.LogSigmoid()
+        self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = x.to(device)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.tanh(self.fc1(x))
+        x = self.tanh(self.fc2(x))
         x = self.fc3(x)
         return x
--- Generator.py
+++ Generator.py
@@ -10,17 +10,17 @@ class Generator(nn.Module):
     def __init__(self, input_dim, action_space_dim):
         super(Generator, self).__init__()
         self.fc1 = nn.Linear(input_dim, 100)
-        torch.nn.init.kaiming_uniform_(self.fc1.weight)
+        #torch.nn.init.kaiming_uniform_(self.fc1.weight)
         self.fc2 = nn.Linear(100, 100)
-        torch.nn.init.kaiming_uniform_(self.fc2.weight)
+        #torch.nn.init.kaiming_uniform_(self.fc2.weight)
         self.fc3 = nn.Linear(100, action_space_dim)
-        torch.nn.init.xavier_uniform_(self.fc3.weight)
+        #torch.nn.init.xavier_uniform_(self.fc3.weight)
         self.logsoftmax = nn.LogSoftmax()
-
-
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU()
     def forward(self, x):
         x = x.to(device)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.tanh(self.fc1(x))
+        x = self.tanh(self.fc2(x))
         x = self.fc3(x)
         return self.logsoftmax(x)
